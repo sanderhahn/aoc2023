468d57e97e66f84806397b8497c431e029b1ffc6
Sander Hahn <sanderhahn@gmail.com>
2023-12-11 09:25:22 +0100

    linter fix

--- day0-template/day.ts
+++ day0-template/day.ts
@@ -1,7 +1,7 @@
-export function part1(input: string): number {
+export function part1(_input: string): number {
   return 0;
 }
 
-export function part2(input: string): number {
+export function part2(_input: string): number {
   return 0;
 }
